dbbb021387ddba5ec6768deb558473327895cb30
PHO <pho@cielonegro.org>
2023-06-09 02:12:48 +0900

    Try inlining constants.d.ts

--- index.d.ts
+++ index.d.ts
@@ -23,7 +23,27 @@
 /// <reference types="./classes.d.ts" />
 /// <reference types="./classes/components.d.ts" />
 /// <reference types="./classes/events.d.ts" />
-/// <reference types="./constants.d.ts" />
 /// <reference types="./enums.d.ts" />
 /// <reference types="./errors.d.ts" />
 /// <reference types="./interfaces.d.ts" />
+
+/**
+ * How many times the server ticks per in-game day.
+ */
+export const TicksPerDay = 24000;
+
+/**
+ * How many times the server ticks per second of real time.
+ */
+export const TicksPerSecond = 20;
+
+/**
+ * A class that provides system-level events and functions.
+ */
+export const system: System;
+
+/**
+ * A class that wraps the state of a world - a set of
+ * dimensions and the environment of Minecraft.
+ */
+export const world: World;
