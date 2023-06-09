9f33484feb6fc9f1be2a03152e154576252e4f0c
PHO <pho@cielonegro.org>
2023-06-09 10:57:24 +0900

    Fix a missing comma in enum TimeOfDay

--- lib/enums.d.ts
+++ lib/enums.d.ts
@@ -268,7 +268,7 @@ export enum SignSide {
  */
 export enum TimeOfDay {
     Day      = 1000,
-    Noon     = 6000
+    Noon     = 6000,
     Sunset   = 12000,
     Night    = 13000,
     Midnight = 18000,
